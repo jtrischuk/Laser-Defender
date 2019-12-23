74dffeca1c737aa669b0fc5ef24c4884a53d24e8
Joel Trischuk <jtrischuk@gmail.com>
2019-12-23 12:51:00 -0800

    Add project files.

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour {
+
+    [SerializeField] float delayInSeconds = 2f;
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        FindObjectOfType<GameSession>().ResetGame();
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadGameOver()
+    {
+
+        StartCoroutine(WaitBeforeLoading());
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator WaitBeforeLoading()
+    {
+
+        yield return new WaitForSeconds(delayInSeconds);
+
+        SceneManager.LoadScene("Game Over");
+
+        
+    }
+}
